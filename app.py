a5db2888d78b224d053d86ea871a7f4161d8fff4
root <root@DESKTOP-7FG4V2F.localdomain>
2023-09-05 09:27:56 -0700

    back-End working

--- app.py
+++ app.py
@@ -21,34 +21,79 @@ def show_all_cupcakes():
     cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
     return jsonify(cupcakes=cupcakes)
 
+@app.route("/api/cupcakes/<int:id>")
+def get_cupcake(id):
+    cupcake_instance = Cupcake.query.get_or_404(id)
+    return jsonify(cupcake=cupcake_instance.serialize())
 
-
-    # cupcakes = Cupcake.query.all()
-    # serialized_cupcakes = [{"id": cupcake.id, "flavor": cupcake.flavor, "size": cupcake.size, "rating": cupcake.rating, "image": cupcake.image} for cupcake in cupcakes]
-    # return jsonify(cupcakes=serialized_cupcakes)
-
-@app.route("/add-cupcake", methods=["GET","POST"])
+@app.route("/api/cupcakes/new", methods=["POST"])
 def add_cupcake():
-    if request.method == "POST":
-        data = request.get_json()  # Get JSON data from the request
-        new_cupcake = Cupcake(
-            flavor=data['flavor'],
-            size=data['size'],
-            rating=data['rating'],
-            image=data.get('image', 'https://tinyurl.com/demo-cupcake')
-        )
-        db.session.add(new_cupcake)
-        db.session.commit()
-        serialized_cupcake = {
-            "id": new_cupcake.id,
-            "flavor": new_cupcake.flavor,
-            "size": new_cupcake.size,
-            "rating": new_cupcake.rating,
-            "image": new_cupcake.image
-        }
-        return (jsonify(cupcake=serialized_cupcake), 201)
-    else:
-       return render_template("create_cupcake.html")
+    # Get JSON data from the request
+    data = request.get_json()
+
+    print("Received JSON data:", data)  # Add this line for debugging
+
+    # Create a new Cupcake instance
+    new_cupcake = Cupcake(
+        flavor=data['flavor'],
+        size=data['size'],
+        rating=data['rating'],
+        image=data.get('image', 'https://tinyurl.com/demo-cupcake')
+    )
+
+    # Add the new cupcake to the database
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    print("Cupcake added to the database:", new_cupcake.serialize())  # Add this line for debugging
+
+    # Return a JSON response with the newly created cupcake data
+    return jsonify(cupcake={
+        "id": new_cupcake.id,
+        "flavor": new_cupcake.flavor,
+        "size": new_cupcake.size,
+        "rating": new_cupcake.rating,
+        "image": new_cupcake.image
+    }), 201  # 201 Created status code
+
+
+@app.route("/api/cupcakes/<int:id>", methods=["PATCH"])
+def update_cupcake(id):
+    # Get JSON data from the request
+    data = request.get_json()
+
+    # Retrieve the cupcake from the database or return a 404 if not found
+    cupcake = Cupcake.query.get_or_404(id)
+
+    # Update the cupcake's attributes
+    cupcake.flavor = data['flavor']
+    cupcake.size = data['size']
+    cupcake.rating = data['rating']
+    cupcake.image = data.get('image', 'https://tinyurl.com/demo-cupcake')
+
+    # Commit the changes to the database
+    db.session.commit()
+
+    # Return a JSON response with the newly updated cupcake data
+    return jsonify(cupcake=cupcake.serialize())
+
+@app.route("/api/cupcakes/<int:id>", methods=["DELETE"])
+def delete_cupcake(id):
+    # Check if the cupcake with the specified ID exists
+    cupcake = Cupcake.query.get(id)
+
+    if cupcake is None:
+        # If the cupcake doesn't exist, return a 404 response
+        return jsonify({"message": "Cupcake not found"}), 404
+
+    # Delete the cupcake from the database
+    db.session.delete(cupcake)
+    db.session.commit()
+
+    # Return a JSON response indicating successful deletion
+    return jsonify({"message": "Deleted"})
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
